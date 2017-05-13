59b4174e94f9341a3608b00a4b947e8fd7f2da56
frankleveque <leveque.frank@gmail.com>
2017-05-13 00:01:48 -0700

    improve style

--- LMC_Compiler.py
+++ LMC_Compiler.py
@@ -1,50 +1,33 @@
-'''
-1 ADD - ADD
-2 SUB - SUBTRACT
-3 STA - STORE 
-5 LDA - LOAD
-6 BRA - BRANCH/JUMP
-7 BRZ - BRANCH IF ZERO
-8 BRP - BRANCH IF POSITIVE
-
-901 INP - INPUT
-902 OUT - OUTPUT
-000 HLT/HCF - HALT/COFFEE
-
-DAT - DATA
-
-'''
-
 import sys
-import re 
 
-opCodes = {}
-opCodes['add'] = 100
-opCodes['sub'] = 200
-opCodes['sta'] = 300
-opCodes['store'] = 300
-opCodes['load'] = 500
-opCodes['lda'] = 500
-opCodes['branch'] = 600
-opCodes['bra'] = 600
-opCodes['branchzero'] = 700
-opCodes['brz'] = 700
-opCodes['branchpositive'] = 800
-opCodes['brp'] = 800
-opCodes['input'] = 901
-opCodes['inp'] = 901
-opCodes['output'] = 902
-opCodes['out'] = 902
-opCodes['halt'] = 000
-opCodes['hlt'] = 000
-opCodes['hcf'] = 000
-opCodes['coffee'] = 000
-opCodes['coffeebreak'] = 000
-opCodes['data'] = None
-opCodes['dat'] = None
-
+opCodes = {
+    'add': 100,
+    'sub': 200,
+    'sta': 300,
+    'store': 300,
+    'load': 500,
+    'lda': 500,
+    'branch': 600,
+    'bra': 600,
+    'branchzero': 700,
+    'brz': 700,
+    'branchpositive': 800,
+    'brp': 800,
+    'input': 901,
+    'inp': 901,
+    'output': 902,
+    'out': 902,
+    'halt': 000,
+    'hlt': 000,
+    'hcf': 000,
+    'coffee': 000,
+    'coffeebreak': 000,
+    'data': None,
+    'dat': None
+}
 memory = ["000"] * 100
 labels = {}
+
 
 def convert_to_lowercase(instructions):
     if isinstance(instructions, str) or isinstance(instructions, int):
@@ -59,22 +42,21 @@ def convert_to_lowercase(instructions):
     return instructions
 
 
-def fixInstructions(instructions):
+def fix_instructions(instructions):
 
     if not instructions:
         return
     initial = len(instructions)
 
-
     # massage inputs
     for i in range(len(instructions)):
         if len(instructions[i]) == 1:
-            instructions[i] = [None] + instructions[i] + ["0"] 
+            instructions[i] = [None + instructions[i] + "0"]
 
-        elif (len(instructions[i]) == 2 and instructions[i][0] in opCodes):
-            instructions[i] = [None] + instructions[i]
+        elif len(instructions[i]) == 2 and instructions[i][0] in opCodes:
+            instructions[i] = [None + instructions[i]]
 
-        elif (len(instructions[i]) == 2 and not (instructions[i][0] in opCodes)):
+        elif len(instructions[i]) == 2 and not (instructions[i][0] in opCodes):
             instructions[i] += ["0"] 
         
     # check
@@ -87,8 +69,7 @@ def fixInstructions(instructions):
     return instructions
 
 
-def stripLabels(instructions):
-  
+def strip_labels(instructions):
     
     for i in range(len(instructions)):
         assert len(instructions[i]) == 3
@@ -102,47 +83,38 @@ def stripLabels(instructions):
         elif first and first in labels:
             print("WARNING: " + first + " already defined - ignoring")
 
-
-
     # strip definitions 
     for i in range(len(instructions)):
         assert len(instructions[i]) == 3
-       
-    
+
         first = instructions[i][0]
         if first:
-            #defining keyword
+            # defining keyword
             if first not in labels.keys():
                 labels[instructions[i][0]] = str(i)
             else:
                 print("Warning: Label " + first + " already defined")
-        
-                    
+
         # remove first index
         instructions[i] = instructions[i][1:]
         assert len(instructions[i]) == 2
-
 
         second = instructions[i][1]
         if second.isalpha() and second not in labels:
             print("Warning: Label " + second + " not defined anywhere - using mailbox 99")
             labels[second] = "99"
             instructions[i][1] = "99" 
-  
 
         for i in range(len(instructions)):
             for j in range(len(instructions[i])):
 
                 try:
-                    if (instructions[i][j] not in opCodes.keys() and not str(instructions[i][j]).isdigit()):
+                    if instructions[i][j] not in opCodes.keys() and not str(instructions[i][j]).isdigit():
                         instructions[i][j] = labels[instructions[i][j]]
 
                 except KeyError:
                     if instructions[i][j] is not None:
                         print("label " + str(instructions[i][j]) + "" + " not defined")
-   
-        
-
 
     # check
     for i in range(len(instructions)):
@@ -151,57 +123,48 @@ def stripLabels(instructions):
     return instructions
 
 
-
-def compileInstruction(instruction):
-    '''
+def compile_instruction(instruction):
+    """
     [OP] [Mailbox/Value]
-    '''
+    """
     assert len(instruction) <= 3 
     try:
         assert instruction[0].isalpha() 
     except AssertionError:
-        print("+++++++++++",instruction[0],"+++++++++++")
+        print("+++++++++++", instruction[0], "+++++++++++")
 
-    
     op = None
-    box = None
     try:
         op = instruction[0]
         opcode = opCodes[op]
         box = instruction[1]
         
-        if isinstance(box,int):
+        if isinstance(box, int):
             box = int(box)
         elif box.isalpha():
             box = labels[box]
 
-
-        if (op == 'hlt' or op == 'halt' or op == 'inp' or op == 'input'):
+        if op == 'hlt' or op == 'halt' or op == 'inp' or op == 'input':
             return opcode
-        elif (op == 'dat' or op == 'data'):
+        elif op == 'dat' or op == 'data':
             return int(box)
         else:
             return opcode + int(box)
            
     except KeyError:
-           print("Warning: Opcode not defined " + str(op) + " - returning HLT")
-           return 0
- 
-
-
+        print("Warning: Opcode not defined " + str(op) + " - returning HLT")
+        return 0
 
 
 if __name__ == "__main__":
-    
 
     if len(sys.argv) < 2:
         print("Needs input file(s)")
         sys.exit(0)
 
-    for a in sys.argv[1:]:
-
+    for file in sys.argv[1:]:
         instructions = [] 
-        with open(a) as f:
+        with open(file) as f:
             data = f.readline()
             while data != "":
                 if data == '\n' or data.startswith('#'):
@@ -211,33 +174,26 @@ if __name__ == "__main__":
                 instructions += [data]
                 data = f.readline()
 
-
         instructions = convert_to_lowercase(instructions)
-        instructions = fixInstructions(instructions)
+        instructions = fix_instructions(instructions)
 
         print("Instructions: ")
-        for a in range(len(instructions)):
-            for b in range(len(instructions[a])):
-                print(instructions[a][b], end="|")
+        for row in list(instructions):
+            for item in row:
+                print(str(item), end="|")
+                pass
             print("")
         print("")
-        instructions = stripLabels(instructions)
+        instructions = strip_labels(instructions)
       
-        
         for i in range(len(instructions)):
-            memory[i] = "%.03i" % compileInstruction(instructions[i])
-
+            memory[i] = "%.03i" % compile_instruction(instructions[i])
 
         assert len(memory) == 100
   
-
-
-        with open(a+'.lmc', 'w+') as out:
+        with open(file + '.lmc', 'w+') as out:
             for line in memory:
                 out.write(str(line) + '\n')
 
 else:
     pass
-
-
-
