01886f5925039a91220b53fbf449cb04839b83cb
frankleveque <leveque.frank@gmail.com>
2017-05-13 03:35:05 -0700

    add copyright notice

--- lmc.cpp
+++ lmc.cpp
@@ -1,3 +1,20 @@
+/*
+    Copyright (C) 2017 Frank Leveque
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    */
+
 #include <iostream>
 #include <fstream>
 #include <cassert>
@@ -27,7 +44,7 @@ int main(int argc, char* argv[])
         std::cout << "reading " << argv[1] << std::endl;
     } 
     if(argc > 2){
-        std::cout << "Ignoring other files";
+        std::cout << "Ignoring other files" << std::endl;
     }
 
     std::ifstream file(argv[1]);
